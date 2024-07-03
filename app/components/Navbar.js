bfde6de795b02b5b6b65aaa88112412527f1f08c
NisargPatel14 <nisjp.14@gmail.com>
2024-07-03 12:39:51 +0530

    Food store Template initialized

---
+++ app/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function navbar() {
+  return (
+    <nav>
+      //temporary navbar
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/products">Products</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
